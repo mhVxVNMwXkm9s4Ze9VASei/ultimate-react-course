bc1281a89ac54d2444c822dcd141618bf46da043
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-07-03 15:13:26 -0500

    Array.reduce()
    vroom

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -171,3 +171,6 @@ const adventureBooks = books
 	.filter((book) => book.genres.includes("adventure"))
 	.map((book) => book.title);
 adventureBooks;
+
+const allBooksPages = books.reduce((sum, book) => sum + book.pages, 0);
+allBooksPages;
